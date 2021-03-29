2fe8563782265ffcfc283bb130c078a8b74151df
guidiamond <guilherme.batista770@gmail.com>
2021-03-29 19:49:51 -0300

    basic turn game impl

---
+++ Boss2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿public class GameManager {
+
+    private static GameManager _instance;
+
+    public enum PlayerTurn {
+        PLAYER1, PLAYER2
+    }
+    public PlayerTurn currentTurn {
+        get; private set;
+    }
+    // Use this for initialization
+    void Start() {
+        currentTurn = PlayerTurn.PLAYER1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public static GameManager GetInstance() {
+        if (_instance == null) {
+            _instance = new GameManager();
+        }
+
+        return _instance;
+    }
+}
---
+++ Boss2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private float velocidade = 5.0f;
+
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start() {
+        gm = GameManager.GetInstance();
+    }
+
+    void Move() {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(inputX , 0 , 0) * Time.deltaTime * velocidade;
+        // Only allow user to go up (jump)
+        if (inputY > 0) {
+            transform.position += new Vector3(0 , 1 , 0) * Time.deltaTime * velocidade;
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+        if (gameObject.tag == "Player1" && gm.currentTurn == GameManager.PlayerTurn.PLAYER1) {
+            Move();
+        }
+        else if (gameObject.tag == "Player2" && gm.currentTurn == GameManager.PlayerTurn.PLAYER2) {
+            Move();
+        }
+    }
+}
