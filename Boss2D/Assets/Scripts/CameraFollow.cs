2ca85a3433ab4e54ba883537d03963f9da992fc0
guidiamond <guilherme.batista770@gmail.com>
2021-04-26 13:47:28 -0300

    minimap working

--- Boss2D/Assets/Scripts/CameraFollow.cs
+++ Boss2D/Assets/Scripts/CameraFollow.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
     // Start is called before the first frame update
     public Transform target1;
     public Transform target2;
@@ -14,13 +13,11 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate() {
         Transform bulletObject = GameObject.FindWithTag("Bullet")?.transform;
-        if (bulletObject) {
-            transform.position = new Vector3(bulletObject.position.x , bulletObject.position.y , transform.position.z);
-        }
-        else if(gm.currentTurn == GameManager.PlayerTurn.PLAYER1) {
+        if ( bulletObject ) {
+            transform.position = new Vector3(bulletObject.position.x, bulletObject.position.y, transform.position.z);
+        } else if ( gm.currentTurn == GameManager.PlayerTurn.PLAYER1 ) {
             transform.position = new Vector3(target1.position.x, target1.position.y, transform.position.z);
-        }
-        else if (gm.currentTurn == GameManager.PlayerTurn.PLAYER2) {
+        } else if ( gm.currentTurn == GameManager.PlayerTurn.PLAYER2 ) {
             transform.position = new Vector3(target2.position.x, target2.position.y, transform.position.z);
         }
     }
